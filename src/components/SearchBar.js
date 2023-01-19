5a45485b6f6c953e422eacfe06fb37d4f65873b1
Marcin S <you@example.com>
2023-01-19 14:34:52 +0100

    Added component state to the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import {
   StyledForm,
   StyledInput,
   StyledSubmit,
-} from "../styles/SearchBar.styles";
+} from '../styles/SearchBar.styles';
 
 const SearchBar = () => {
+  const [phrase, setPhrase] = useState('');
+  const handleClick = (e) => {
+    setPhrase(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPhrase('');
+  };
+
   return (
-    <StyledForm>
-      <StyledInput type="text" placeholder="Type IP Address or URL" />
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledInput
+        type='text'
+        placeholder='Type IP Address or URL'
+        onChange={handleClick}
+        value={phrase}
+      />
       <StyledSubmit>Search</StyledSubmit>
     </StyledForm>
   );
